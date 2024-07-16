4c787609c715bd88e6580592e21db704ce65b3d6
Ivan85AC <Chernyhia@rambler.ru>
2024-07-16 12:42:07 +0500

    Update Module_3_5.py

--- Module_3_5.py
+++ Module_3_5.py
@@ -1,4 +1,5 @@
 def get_multiplied_digits(number):
+    number = int(number)
     str_number = str(number)
     first = int(str_number[0])
     if len(str_number) < 2:
@@ -7,4 +8,5 @@ def get_multiplied_digits(number):
         return first * get_multiplied_digits(int(str_number[1:]))
 
 
+num = input('Введите число: ')
 print(get_multiplied_digits(40203))
